a9043a5da4a4eb3b25e7f3a29c14f91030a0d281
Wen-Long Sun <8915336+thun88@users.noreply.github.com>
2023-08-20 17:11:47 +0200

    2nd git, list updated

--- htaccess_ip_blocker.py
+++ htaccess_ip_blocker.py
@@ -3,18 +3,14 @@ output_file = ".htaccess"
 
 with open(input_file, "r") as ip_list:
     ip_addresses = ip_list.readlines()
+
 htaccess_code = "#######   HTAccessIPBlocker BEGIN    #######\n"
-htaccess_code += "<IfModule mod_rewrite.c>\n    RewriteEngine On\n\n"
 
 for ip_address in ip_addresses:
     ip_address = ip_address.strip()
-    htaccess_code += f"    RewriteCond %{{REMOTE_ADDR}} ^{ip_address}$ [OR]\n"
+    htaccess_code += f"deny from {ip_address}\n"
 
-# Remove the last [OR] from the last condition
-htaccess_code = htaccess_code.rstrip("[OR]\n")
-
-htaccess_code += "\n    RewriteRule ^ - [F]\n</IfModule>"
-htaccess_code += "\n#######   HTAccessIPBlocker END    #######"
+htaccess_code += "#######   HTAccessIPBlocker END    #######"
 
 with open(output_file, "w") as htaccess:
     htaccess.write(htaccess_code)
